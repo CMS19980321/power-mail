225d031df467f4aa4f00db57a73ed70da36bce03
Administrator <gitlab_admin_e31d59@example.com>
2026-07-04 21:13:06 +0800

    20260704代码提交:
    商城用户购物系统:查询会员是否绑定手机号接口

--- power-business/member-service/src/main/java/com/hncu/controller/MemberController.java
+++ power-business/member-service/src/main/java/com/hncu/controller/MemberController.java
@@ -1,12 +1,15 @@
 package com.hncu.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.hncu.domain.Member;
 import com.hncu.model.Result;
 import com.hncu.service.MemberService;
+import com.hncu.util.AuthUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -35,5 +38,22 @@ public class MemberController {
     public Result<String> mofigyMemberInfo(@RequestBody Member member){
         Boolean modified = memberService.modifyMemberInfoByOpenId(member);
         return Result.handle(modified);
+    }
+
+    /**
+     * 查询会员是否绑定手机号码
+     * @return
+     */
+    @ApiOperation("查询会员是否绑定手机号")
+    @GetMapping("isBindPhone")
+    public Result<Boolean> loadMemberOpenId(){
+        //获取会员的openId
+        String memberOpenId = AuthUtils.getMemberOpenId();
+        //根据openId获取会员详情
+        Member member = memberService.getOne(new LambdaQueryWrapper<Member>()
+                .eq(Member::getOpenId, memberOpenId)
+        );
+
+        return Result.success(StringUtils.hasText(member.getUserMobile()));
     }
 }
